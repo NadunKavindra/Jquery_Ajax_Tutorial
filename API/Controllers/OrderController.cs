a31a1470d43cc93063238e10fa99de43b2a21e38
nadun.kavindra@gmail.com <nadun.kavindra@gmail.com>
2019-03-22 21:45:50 +0530

    Implimented order Get and Post actions

---
+++ API/Controllers/OrderController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderContext _context;
+        public OrderController(OrderContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Order
+        [HttpGet]
+        public IEnumerable<Order> Get()
+        {
+            return _context.Orders;
+        }
+
+        // GET: api/Order/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetOrder([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var order = await _context.Orders.SingleOrDefaultAsync(m => m.id == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return Ok(order);
+        }
+
+        // POST: api/Order
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetOrder", new { id = order.id }, order);
+        }
+
+        // PUT: api/Order/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
